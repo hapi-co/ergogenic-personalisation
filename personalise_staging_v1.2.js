a350b428fdbf01a0495185eca8ec6aac35543f9f
John <hello@gethapi.me>
2021-11-16 20:41:50 +1100

    commit

--- personalise_staging_v1.2.js
+++ personalise_staging_v1.2.js
@@ -2,42 +2,46 @@ $( document ).ready(function() {
     // console.log( "ready 2!" );
 
     const urlParams = new URLSearchParams(window.location.search);
+    // console.log(urlParams);
+
+    // Name values
+    const name_param = urlParams.get('name');
+    console.log("name param =", name_param);
+
+    // Category values
+    const category_param = urlParams.get('category');
+    console.log("category param =", category_param);
+
+    // Experience values
     const experience_param = urlParams.get('experience');
     console.log("experience param =", experience_param);
 
-    // var is_noob = urlParams.has('experience'); // true (boolean)
-    // console.log("experience_true");
-    // var noob_val = urlParams.get('experience'); //success
-    // console.log("experience =", noob_val);
-
-    var experience_val = urlParams.get('experience'); // true (string)
+    // var experience_val = urlParams.get('experience'); // true (string)
     // console.log("experience_true");
     var experience_string = urlParams.get('experience'); // NULL
 
-    // if (urlParams.has('experience')) {
-      // console.log("success");
-      // var noob = document.getElementById("noob");
-      // noob.style.display = "block";
-      // console.log("noob true");
-    // }
+    // Update name
+    var name_string = urlParams.get('name'); // NULL
+    console.log(name_string);
+    $("#name").text("Name: " + name_string);
+
+    // Update category
+    var category_string = urlParams.get('category'); // NULL
+    console.log(category_string);
+    $("#category").text("Category: " + category_string);
+
 
     if (experience_string == 'noob') {
       console.log("NOOB!");
-      noob.style.display = "block";
-      avg.style.display = "none";
-      advanced.style.display = "none";
+      $("#experience").text("Experience: " + "YOU'RE A NOOB!");
 
     } else if (experience_string == 'avg') {
       console.log("avg true");
-      noob.style.display = "none";
-      avg.style.display = "block";
-      advanced.style.display = "none";
+      $("#experience").text("Experience: " + "YOU'RE AVERAGE!");
 
     } else if (experience_string == 'advanced') {
       console.log("advanced true");
-      noob.style.display = "none";
-      avg.style.display = "none";
-      advanced.style.display = "block";
+      $("#experience").text("Experience: " + "YOU'RE ADVANCED!");
 
     }
 
