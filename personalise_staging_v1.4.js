6c4637c8d35e3dd89b016f87d3e78a55c6bc57e4
John <hello@gethapi.me>
2021-12-07 22:52:03 +1100

    commit

--- personalise_staging_v1.4.js
+++ personalise_staging_v1.4.js
@@ -20,7 +20,7 @@
     const name_param = urlParams.get('name');
     console.log("name param =", name_param);
 
-    // Optomised values
+    // Optimised values
     // Values: notvery, somewhat, psychonaut
     const optimised_param = urlParams.get('optimised');
     console.log("optimised param =", optimised_param);
@@ -42,90 +42,159 @@
     const experience_param = urlParams.get('experience');
     console.log("experience param =", experience_param);
     // Test w/ out last line, seems like a duplicate...
-    var experience_string = urlParams.get('experience');
+    // var experience_string = urlParams.get('experience');
 
     // Update name
     var name_string = urlParams.get('name'); // NULL
-    console.log(name_string);
+    console.log("Update name to:",name_string);
     $("#name").text(name_string);
     // $("#name_01").text(name_string);
 
     // Update category
     var category_string = urlParams.get('category');
-    console.log(category_string);
+    console.log("Update category to:", category_string);
     $("#category").text(category_string);
 
-    // Update priority
-    var priority_string = urlParams.get('priority');
-    console.log(priority_string);
-    $(".subcategory").text(priority_string);
+    // Update subcategory
+    // var subcategory_string = urlParams.get('subcategory');
+    // console.log("Update subcategory to:", subcategory_string);
+    // $(".subcategory").text(subcategory_string);
 
     // Experience / level of optimisation from 1-3
     // Hero section
-    if (experience_string == 'mush') {
-      console.log("mush = true");
-      $("#experience").text("Optimised: " + "YOU'RE A NOOB!");
+    if (optimised_param == 'notvery') {
+      console.log("Optimised: Not Very = true");
+      // $("#experience").text("Optimised: " + "YOU'RE A NOOB!");
       $("#optimised_01").css('opacity', '1');
       $("#optimised_02").css('opacity', '0.5');
       $("#optimised_03").css('opacity', '0.5');
       $("#optimised_01").css('border', '4px solid #395C56');
 
-    } else if (experience_string == 'noob') {
-      console.log("noob = true");
-      $("#experience").text("Optimised: " + "YOU'RE AVERAGE!");
+    } else if (optimised_param == 'somewhat') {
+      console.log("Optimised = Somewhat");
+      // $("#experience").text("Optimised: " + "YOU'RE AVERAGE!");
       $("#optimised_01").css('opacity', '0.5');
       $("#optimised_02").css('opacity', '1');
       $("#optimised_03").css('opacity', '0.5');
       $("#optimised_02").css('border', '4px solid #395C56');
 
-    } else if (experience_string == 'advanced') {
-      console.log("advanced = true");
-      $("#experience").text("Optimised: " + "YOU'RE ADVANCED!");
+    } else if (optimised_param == 'psychonaut') {
+      console.log("Optimised: Psychonaut = true");
+      // $("#experience").text("Optimised: " + "YOU'RE ADVANCED!");
       $("#optimised_01").css('opacity', '0.5');
       $("#optimised_02").css('opacity', '0.5');
+      $("#optimised_03").css('opacity', '1');
       $("#optimised_03").css('border', '4px solid #395C56');
-
     }
 
+    // Show / hide blocks
+    // $("#id").show();
+    // $("#id").hide();
+    // Checklist section
+    // Noots section
+      // #noots_block
+    // Ebook section
+      // #ebook_block
+    // Playlist section
+      // #playlist_block
+    // Masterclass section
+      // #masterclass_block
+    // Custom Stack section
+      // #custom_stack_block
+
     // Subcategory focus area
-    if (priority_string == 'focus') {
+    if (subcategory_param == 'focus') {
       console.log("focus = true");
+      // Checklist section
       $("#checklist_title").text("Focus-Enhancement");
+      // Noots section
       $("#noots_products_title").text("Focus");
+      $("#noots_products_subheading").text("Noots subheading");
+      $("#products_focus").show();
+      $("#products_memory").hide();
+      $("#products_stress").hide();
+      $("#products_mood").hide();
+      // Ebook section
       $("#ebook_title").text("Improve Focus");
+      $("#ebook_subheading").text("Ebook subheading");
+      // Playlist section
       $("#playlist_title").text("Focus-Enhancement");
+      // Masterclass section
+      $("#masterclass_subheading").text("Masterclass subheading");
+      // Custom Stack section
       $("#custom_stack_title").text("Focus-Enhancement");
 
-    } else if (priority_string == 'memory') {
+    } else if (subcategory_param == 'memory') {
       console.log("memory = true");
+      // Checklist section
       $("#checklist_title").text("Memory-Enhancement");
+      // Noots section
       $("#noots_products_title").text("Memory");
+      $("#noots_products_subheading").text("Noots subheading");
+      $("#products_focus").hide();
+      $("#products_memory").show();
+      $("#products_stress").hide();
+      $("#products_mood").hide();
+      // Ebook section
       $("#ebook_title").text("Improve Memory");
+      $("#ebook_subheading").text("Ebook subheading");
+      // Playlist section
       $("#playlist_title").text("Memory-Enhancement");
+      // Masterclass section
+      $("#masterclass_subheading").text("Masterclass subheading");
+      // Custom Stack section
       $("#custom_stack_title").text("Memory-Enhancement");
 
-    } else if (priority_string == 'stress') {
+    } else if (subcategory_param == 'stress') {
       console.log("stress = true");
+      // Checklist section
       $("#checklist_title").text("Stress-Reduction");
+      // Noots section
       $("#noots_products_title").text("Stress-Reduction");
+      $("#noots_products_subheading").text("Noots subheading");
+      $("#products_focus").hide();
+      $("#products_memory").hide();
+      $("#products_stress").show();
+      $("#products_mood").hide();
+      // Ebook section
       $("#ebook_title").text("Reduce Stress");
+      $("#ebook_subheading").text("Ebook subheading");
+      // Playlist section
       $("#playlist_title").text("Stress-Reduction");
+      // Masterclass section
+      $("#masterclass_subheading").text("Masterclass subheading");
+      // Custom Stack section
       $("#custom_stack_title").text("Stress-Reduction");
 
-    } else if (priority_string == 'mood') {
+    } else if (subcategory_param == 'mood') {
       console.log("mood = true");
+      // Checklist section
       $("#checklist_title").text("Mood-Boosting");
+      // Noots section
       $("#noots_products_title").text("Mood-Boosting");
+      $("#noots_products_subheading").text("Noots subheading");
+      $("#products_focus").hide();
+      $("#products_memory").hide();
+      $("#products_stress").hide();
+      $("#products_mood").show();
+      // Ebook section
       $("#ebook_title").text("Boost your Mood");
+      $("#ebook_subheading").text("Ebook subheading");
+      // Playlist section
       $("#playlist_title").text("Mood-Boosting");
+      // Masterclass section
+      $("#masterclass_subheading").text("Masterclass subheading");
+      // Custom Stack section
       $("#custom_stack_title").text("Mood-Boosting");
 
-    } else if (priority_string == 'other') {
+    } else if (subcategory_param == 'other') {
       console.log("other = true");
-      // Script here
-      // Hide container
+      console.log("hiding Noots section");
+      $("#noots_block").hide();
       // Show 1-1 coaching / cust stack service
-
+      // Figure out how to MOVE cust stack to Noots sect, or duplicate sect & use classes instead of IDs on both so it's seamless. But still, there will be a ton of varients... what is a simple solution here?
+      // Do I need to interact w/ the DOM?
+      // Ask Timon, maybe?
     }
 
 });
@@ -140,10 +209,10 @@ jQuery("#playVideo").on('click', function() {
          $(".embed-container").css({ opacity: 1 });
          $(".embed-container").css({ height: "100%" });
          $(".embed-container").css({ paddingBottom: "56.25%" });
-         console.log("video opacity set to 100%");
-         console.log("video height set to 100%");
-         console.log("video padding-bottom set to 56.25%");
-         console.log("video playing...");
+         // console.log("video opacity set to 100%");
+         // console.log("video height set to 100%");
+         // console.log("video padding-bottom set to 56.25%");
+         // console.log("video playing...");
        },
        // Num of miliseconds to hide for. 1,000 = 1 sec.
        350);
